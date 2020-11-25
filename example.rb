b44970c2e48f52a3126d886aa694ea8fcee5a712
Henok <henatan99@gmail.com>
2020-11-25 03:14:33 +0300

    creates TicTacToe class in the main file - It encompasses player and board objects - Methods are   - create_players   - create_board   - pick_symbol   - set_begginer   - set_current_player   - check_move   - update_board   - win_move and   - win_move_all win_move and win_move_all methods are applied to evaluate if a move has winning condition

--- example.rb
+++ example.rb
@@ -1,137 +1,25 @@
-class TicTacToe
-    attr_accessor :name, :player
-  
-    def initialize(name)
-      @name = name
-      @player = []
-      puts "Game initiated: Game name is #{name}"
-    end
-  
-    def add_player(player)
-      @player << player
-      player.game = self
-    end
-  
-    def starter(beginner, _max_moves)
-      @beginner = beginner
-      break unless @beginner.to_s != @player[0].name.to_s
-  
-      a = @player[0]
-      @player[0] = @player[1]
-      @player[1] = a
-    end
-  
-    def move(picked_cell)
-      @picked_cell = picked_cell
-    end
-  
-    def current_player(move_no)
-      @move_no = move_no
-      @current_player = @player[move_no % 2]
-    end
-  end
-  
-  class Player
-    attr_accessor :name, :game, :sym
-  
-    def initialize(name)
-      @name = name
-    end
-  
-    def pick_symbol(sym)
-      @sym = sym
-    end
-  end
-  
-  class Board
-    attr_accessor :row
-  
-    def initialize(symbol)
-      @symbol = []
-      9.times do
-        @symbol << ' '
-      end
-    end
-  
-    def update_symbol(picked_cell, player)
-      @symbol[picked_cell - 1] = player.sym
-    end
-  
-    def display_board
-      y = @symbol[0].to_s
-      2.times do |cell|
-        x = ' |' + @symbol[cell + 1].to_s
-        y += x
-      end
-      puts y
-      (@row - 1).times do |h|
-        rc = (h + 1) * @row
-        y = @symbol[rc].to_s
-        puts '-' * (@row * 3 - 1)
-        (@row - 1).times do
-          x = ' |' + @symbol[rc + 1].to_s
-          y += x
-          rc += 1
-        end
-        puts y
-      end
-    end
-end
-  
+def win_move(i, check)
+    $adj = [[2, 4, 5, 3, 7, 9], [1, 3, 5, 8], [2, 5, 6, 1, 7, 9], [1, 5, 7, 6], [1, 2, 3, 4, 6, 7, 8, 9], [3, 5, 9, 4], [4, 5, 8, 1, 3, 9], [5, 7, 9, 2], [5, 6, 8, 1, 3, 7]]
+    $adj2 = [[3, 7, 9], [8], [1, 7, 9], [6], [], [4], [1, 3, 9], [ 2], [1, 3, 7]]
+    if $adj[i-1].any?(check)
+      diff = check - i  
+      if $adj2[i-1].any?(check)
+        return (i + check) / 2
+      elsif diff == 1 
+        return check%3 == 0 ? check - 2*diff : check + diff 
+      elsif diff.positive? && (check+diff) <= 9 && i!=5
+        return check+diff
+      elsif diff.positive? 
+        return check - 2*diff
+      elsif diff == -1
+        return check%3 == 1 ? check - 2*diff : check+diff
+      elsif diff.negative? && (check+diff) > 0 && i!=5
+        return check+diff   
+      elsif diff.negative?  
+        return check - 2*diff                  
+     end 
+    else 
+      puts "No win move"
+    end     
+end 
 
-print welcome_text
-
-puts 'Please Insert the Match Name'
-
-match_name = gets.chomp
-
-game = TicTacToe.new(match_name)
-
-puts 'Insert name of players: '
-
-player1 = Player.new(gets.chomp)
-player2 = Player.new(gets.chomp)
-
-game.add_player(player1)
-game.add_player(player2)
-
-puts 'The players are: '
-puts player1.name
-puts player2.name
-
-puts 'Now you insert the row or column numbers of your choice: '
-
-rows = gets.chomp.to_i
-board = Board.new(rows)
-
-board.display_board
-
-puts 'Ok ' + player1.name + ' please pick your symbol: '
-
-sym1 = player1.pick_symbol(gets.chomp.to_s)
-
-puts 'Ok ' + player2.name + ' please pick your symbol: '
-
-sym2 = player2.pick_symbol(gets.chomp.to_s)
-
-puts player1.name + ' choses ' + sym1
-puts player2.name + ' choses ' + sym2
-
-puts 'Game is ready. Now pick the player for the first move: '
-
-beginner = gets.chomp.to_s
-
-game.starter(beginner, 25)
-
-puts player1.name + ' will start'
-
-rows.times do |i|
-  curr_player = game.current_player(i)
-
-  puts '@ ' + curr_player.name + ', now your turn. Pick a cell: '
-  pick = gets.chomp
-
-  puts board.update_symbol(pick.to_i, curr_player)
-
-  board.display_board
-end
